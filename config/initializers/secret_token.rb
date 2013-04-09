5a072adea98bff631355f2053bfd8466f93a5b59
Emily <emily@10gen.com>
2013-04-09 08:36:23 -0400

    Skeleton app with half-baked authentication for HW3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Courszilla::Application.config.secret_token = 'd8a040d8e535f4a59ad853696410fb6a104a6f912beb1157456a2c25fe4425340752093b72e01a8f6a5a0225281ec7b21b64a0db2d041e72153f2e0ff7f43e27'
